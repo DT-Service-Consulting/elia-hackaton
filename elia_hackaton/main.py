612305192513caaf0498f8969bfa145c0dbfd1ad
Marco <marco@mdigennaro.net>
2025-03-18 13:07:52 +0100

    streamlit

--- elia_hackaton/main.py
+++ elia_hackaton/main.py
@@ -6,4 +6,15 @@ from scipy.optimize import linprog
 
 from elia_hackaton.core import fetch_data
 
-fetch_data()
+#fetch_data()
+import torch
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")  # Use NVIDIA GPU
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")  # Use Apple Metal (macOS)
+else:
+    device = torch.device("cpu")  # Default to CPU
+
+print(f"Using device: {device}")
+
